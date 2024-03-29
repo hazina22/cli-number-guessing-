da641eeb5f81f133d12db99a1b41c665bcf0104b
hazina <sheikhsaadi269@gmail.com>
2024-03-29 17:19:22 +0500

    created number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log(randomNumber);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number 1-6:",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congratulations you  guessed a right number");
+}
+else {
+    console.log("you guessed a wrong number");
+}
